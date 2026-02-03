638d9e99dd0a216fee0f2554c93d803289e95179
agstydva <agastyadava01@gmail.com>
2026-02-03 10:56:33 +0700

    update desc

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -350,7 +350,16 @@ const Dashboard = () => {
 
                 {/* SKILLS SECTION */}
                 <section id="skills" className="py-10 px-4 md:px-10 lg:px-20 xl:px-40">
-                    <h2 className="mb-12 text-center text-4xl font-bold text-gray-900">Skills</h2>
+                    {/* <h2 className="mb-12 text-center text-4xl font-bold text-gray-900">Skills</h2> */}
+
+                    <div className="flex flex-col items-center justify-center gap-3">
+                        <h2 className="text-3xl md:text-4xl font-bold text-gray-900 text-center">
+                            Skills
+                        </h2>
+                        <p className="text-slate-500 font-medium max-w-6xl mx-auto mb-12 leading-relaxed text-sm sm:text-base text-center">
+                            Tools I've Placed My Hands On.
+                        </p>
+                    </div>
                     
                     <div className="flex justify-center">
                         <div className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-10 gap-x-4 gap-y-8 justify-items-center w-full max-w-7xl">
